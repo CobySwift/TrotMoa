46d0fbbb580ca42f4bb84365a84b336b36dab4e2
coby5502 <57849386+coby5502@users.noreply.github.com>
2023-02-24 01:17:36 +0900

    Initial Commit

---
+++ TrotMoa/TrotMoaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrotMoaApp.swift
+//  TrotMoa
+//
+//  Created by COBY_PRO on 2023/02/24.
+//
+
+import SwiftUI
+
+@main
+struct TrotMoaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
